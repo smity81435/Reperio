f9f7a91a9fab548357ce881f36f0e81a34777c6d
smity81435 <smithdouglas25@gmail.com>
2019-02-09 16:49:09 -0700

    changes

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,6 +3,7 @@ import firebase from 'firebase'
 
 
 
+
 var db;
 var importList;
 
@@ -27,47 +28,13 @@ export function addAnswer(newAns){
   return db.collection("answers").add(newAns);
 }
 
-export function listen(nodeList1,nodeList2){
-    var node;
+export function listen(callback){
     db.collection("answers").onSnapshot(snapshot => {
-        snapshot.docChanges().forEach( change => {
-            if (change.type === "added") {
-                //console.log("New: ", change.doc.data());
-                node= change.doc.data();
-                if(node.ans == 1){
-                    nodeList1.push({
-                        id: nodeList1.length+1,
-                        _color: '#6ec6ff',
-                    });
-                }else{
-                    nodeList2.push({
-                        id: nodeList2.length+1,
-                        _color: '#64ffda',
-                    });
-                }
-            }
-            if (change.type === "modified") {
-                console.log("Modified: ", change.doc.data());
-            }
-            if (change.type === "removed") {
-                console.log("Removed: ", change.doc.data());
-            }
-
+        snapshot.docChanges().forEach((change) => {
+            callback(change);
         });
-        
         console.log(importList);
-        
     });
-}
-//Gets the Decks collection
-export function getDecks() {
-    return db.collection("decks").get();
-}
-
-//gets all the cards for a specified deck
-export function getCards(deckId) {
-    return db.collection("cards").where("parentId", "==", deckId)
-        .get();
 }
 
 export var db;
--- src/views/RepDisplay.vue
+++ src/views/RepDisplay.vue
@@ -2,21 +2,24 @@
   <div class="display">
     <h1 class="questionheader">Would you rather??</h1>
     <div class="visualization">
-      <div class="datagroup group1">
-        <h2 class="groupname groupname1">Eat Guano</h2>
-        <d3-network class="nodeGroup1" ref='net' :net-nodes="nodeList1"  :options="options"
-         />
-         <p class="nodecount count1"  
-            v-bind:style="
-              [(this.nodeList1.length >= this.nodeList2.length) ? {'color':'rgb(28, 255, 16)', 'text-shadow': '0px 0px 10px white', 'font-size':'50px'} : {'color':'rgb(211, 102, 102)'}]"
-            >{{this.nodeList1.length}}</p>
-      </div>
-      <div class="datagroup group2">
-        <h2 class="groupname groupname2">Snort Wasabi</h2>
-        <d3-network class="nodeGroup1" ref='net' :net-nodes="nodeList2"  :options="options" />
-        <p class="nodecount count2" v-bind:style="
-              [(this.nodeList2.length >= this.nodeList1.length) ? {'color':'rgb(28, 255, 16)', 'text-shadow': '0px 0px 10px white', 'font-size':'50px'} : {'color':'rgb(211, 102, 102)'}]"
-              >{{this.nodeList2.length}}</p>
+      <div
+        class="datagroup"
+        v-for="(count, index) in results"
+        :key="index">
+        <h2 class="groupname">{{responses[index]}}</h2>
+        <d3-network
+          class="nodegroup"
+          ref="net"
+          :net-nodes="buildNodeList(count, index)"
+          :net-links="[]"
+          :options="options"/>
+        <p
+          :class="{
+            nodecount: true,
+            winning: isWinning(count),
+          }">
+          {{ results[index] }}
+        </p>
       </div>
     </div>
   </div>
@@ -27,57 +30,41 @@
 import D3Network from 'vue-d3-network'
 import * as Api from "@/api/Api.js"
 
+const colors = ['#6ec6ff', '#64ffda'];
+const responses= ['Eat Guano','Snort Wasabi'];
+
 export default {
   components:{
     D3Network,
   },
   data(){
-    return{
+    return {
+      responses: responses,
       importList: [
         
       ],
-      nodeList1: [
-        // { id: 1,_color: '#6ec6ff'},
-        // { id: 2,_color: '#6ec6ff'},
-        // { id: 3,_color: '#6ec6ff'},
-        // { id: 4,_color: '#6ec6ff'},
-        // { id: 5,_color: '#6ec6ff'},
-      ],
-      nodeList2:[
-        // { id: 1,_color: '#64ffda'},
-        // { id: 2,_color: '#64ffda'},
-        // { id: 3,_color: '#64ffda'},
-        // { id: 4,_color: '#64ffda'},
-        // { id: 5,_color: '#64ffda'},
-        // { id: 6,_color: '#64ffda'},
-        // { id: 7,_color: '#64ffda'},
-        // { id: 8,_color: '#64ffda'},
-        // { id: 9,_color: '#64ffda'},
-        // { id: 10,_color: '#64ffda'},
-        // { id: 11,_color: '#64ffda'},
-      ],
-      links: [
-        { sid: 1, tid: 2 },
-        { sid: 2, tid: 8 },
-        { sid: 3, tid: 4 },
-        { sid: 4, tid: 5 },
-        { sid: 5, tid: 6 },
-        { sid: 7, tid: 8 },
-        { sid: 5, tid: 8 },
-        { sid: 3, tid: 8 },
-        { sid: 7, tid: 9 }
-      ],
+      // The number of responses for each option
+      results: [],
       nodeSize:40,
       canvas:false,
       deg:10,
     }
   },
-  created(){
-    //this.$api.dispatch(initRealtimeListener);
-    Api.listen(this.nodeList1, this.nodeList2);
-  },
-    methods:{
 
+  methods: {
+    buildNodeList(count, index) {
+      var nodes = [];
+      for (var i = 0; i < count; i++) {
+        nodes.push({
+          id: i,
+          _color: colors[index],
+        });
+      }
+      return nodes;
+    },
+    isWinning(count) {
+      return Math.max(...this.results) === count;
+    },
   },
   computed:{
     options(){
@@ -86,21 +73,40 @@ export default {
         size:{ w:400, h:400},
         nodeSize: this.nodeSize,
         nodeLabels: false,
-        canvas: this.canvas
+        canvas: this.canvas,
       }
     }
   },
-  mounted(){
-    if(this.nodeList1.length > this.nodeList2.length){
-      num1.classList.add('winning');
-      num2.classList.remove('winning');
-    }else if(this.nodeList1.length < this.nodeList2.length){
-      num1.classList.remove('winning');
-      num2.classList.add('winning');
+  mounted() {
+    // Initialize number of responses in results data object
+    for (var i = 0; i < this.responses.length; i++) {
+      this.results.push(0);
     }
-      //console.log("this is the import list "+this.importList);
+
+    // Update node lists every time an answer is received
+    Api.listen((change) => {
+      console.log(change);
+      if (change.type === "added") {
+        //console.log("New: ", change.doc.data());
+        var node = change.doc.data();
+        if (node.ans < 0 || node.ans >= this.results.length) {
+          console.log('invalid answer number');
+          return;
+        }
+        // Add an option
+        this.results[node.ans] += 1;
+        this.$forceUpdate();
+      }
+
+      if (change.type === "modified") {
+        console.log("Modified: ", change.doc.data());
+      }
+      if (change.type === "removed") {
+        console.log("Removed: ", change.doc.data());
+      }
+    });
+    //console.log("this is the import list "+this.importList);
   }
-  
 }
 </script>
 <style lang="scss">
@@ -172,8 +178,14 @@ ul.menu li{
   margin-top: 1em;
   position: relative;
 }
-.winning{
+
+.nodecount {
+  color: rgb(211, 102, 102);
+}
+.nodecount.winning {
   color: rgb(28, 255, 16);
+  font-size: 50px;
+  text-shadow: 0px 0px 10px white;
 }
 </style>
 
